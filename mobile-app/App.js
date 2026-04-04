4b739386e640e50b6eec7067a442f546bc756b16
nuha-riwaz <riwaznuha@gmail.com>
2026-04-04 14:45:11 +0530

    navigation bar updated

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import RoomStack from "./navigation/RoomStack";
-import { View, Text } from "react-native";
-
-const Tab = createBottomTabNavigator();
-
-function HomePlaceholder() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen (common)</Text>
-    </View>
-  );
-}
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppTabs from "./navigation/AppTabs";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={HomePlaceholder} />
-        <Tab.Screen name="Rooms" component={RoomStack} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppTabs />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
